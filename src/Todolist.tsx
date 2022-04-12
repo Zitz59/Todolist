88574c4358503e1138fd7f39c5e01950e83a4ae5
Zitz59 <dondongudon@gmail.com>
2022-04-12 10:40:54 +0300

    03. Add input tasks functions and refactor

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import {FilterValuesType} from "./App";
+import React, {useState, KeyboardEvent, ChangeEvent} from 'react';
+import {FilterValuesType} from './App';
+
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
@@ -10,14 +11,39 @@ export type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskID: number) => void
-    changeFilter:(filter:FilterValuesType) =>void
+    addTask: (title: string) => void
+    removeTask: (taskID: string) => void
+    changeFilter: (filter: FilterValuesType) => void
+
 
 }
 
-
 export const TodoList = (props: PropsType) => {
+    const [title, setTitle] = useState<string>('');
 
+    const onClickAddTask = () => {
+        console.log('add', title)
+        const trimmedTitle = title.trim()
+        console.log(trimmedTitle)
+        if (trimmedTitle) {
+            console.log('if', trimmedTitle, props.addTask)
+            props.addTask(trimmedTitle)
+            setTitle('')
+        }
+    }
+
+    const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            onClickAddTask()
+        }
+    }
+    const onAllClickHandler = () => props.changeFilter('all')
+    const onActiveClickHandler = () => props.changeFilter('active')
+    const onCompletedClickHandler = () => props.changeFilter('completed')
     const tasksListItems = props.tasks.map(t => { //t => {id:1, title:"HTML", isDone:true}
         const removeTask = () => props.removeTask(t.id)
         return (
@@ -28,26 +54,25 @@ export const TodoList = (props: PropsType) => {
 
         )
     })
-
     return (
         <div>
+            <h3>{props.title}</h3>
             <div>
-                <h3>{props.title}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    {tasksListItems}
-                </ul>
-                <div>
-                    <button onClick={()=> props.changeFilter("all")}>All</button>
-                    <button onClick={()=> props.changeFilter("active")}>Active</button>
-                    <button onClick={()=> props.changeFilter("completed")}>Completed</button>
-                </div>
+                <input
+                    value={title}
+                    onChange={onNewTitleChangeHandler}
+                    onKeyPress={onKeyPressHandler}/>
+                <button onClick={onClickAddTask}>+</button>
+            </div>
+            <ul>
+                {tasksListItems}
+            </ul>
+            <div>
+                <button onClick={onAllClickHandler}>All</button>
+                <button onClick={onActiveClickHandler}>Active</button>
+                <button onClick={onCompletedClickHandler}>Completed</button>
             </div>
         </div>
-    );
-};
 
-export default TodoList;
+    );
+}
