5ebf01aefa2c4dcbe017547d4f0f223e50dd5143
Zitz59 <dondongudon@gmail.com>
2022-05-01 00:00:48 +0300

    Updates

---
+++ precache-manifest.6a90a28c6f982e0179ee59b49a98b11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a096556ce2c132d83da8a8362e43b1",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "6f38ba08025cc426b1ce",
+    "url": "/Todolist/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "8687e3895d0165189ec3",
+    "url": "/Todolist/static/js/2.38b57ddc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.38b57ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f38ba08025cc426b1ce",
+    "url": "/Todolist/static/js/main.348839e2.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
