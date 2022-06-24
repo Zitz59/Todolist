828c8559809a5513afa9a962b12d696c70f74975
Zitz59 <dondongudon@gmail.com>
2022-06-24 18:26:09 +0300

    09 add  changeTaskTitleAC and ADD-TODOLIST case

--- src/state/task-reducer.ts
+++ src/state/task-reducer.ts
@@ -1,5 +1,6 @@
 import {TasksStateType} from '../App';
 import {v1} from 'uuid';
+import {AddTodolistACType} from './todolists-reducer';
 
 export type RemoveTaskType = ReturnType<typeof removeTaskAC>
 
@@ -7,9 +8,14 @@ export type AddTaskType = ReturnType<typeof addTaskAC>
 
 export type ChangeTaskStatusType = ReturnType<typeof changeTaskStatusAC>
 
+export type ChangeTaskTitleType = ReturnType<typeof changeTaskTitleAC>
+
 
 type ActionsType = RemoveTaskType
-    | AddTaskType | ChangeTaskStatusType
+    | AddTaskType
+    | ChangeTaskStatusType
+    | ChangeTaskTitleType
+    | AddTodolistACType //from todolist-reducer
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
@@ -32,6 +38,22 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
                     isDone: action.isDone
                 } : t)
             }
+        case 'CHANGE-TASK-TITLE':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].map(t => t.id === action.taskId ? {
+                    ...t,
+                    title: action.title
+                } : t)
+            }
+
+        case 'ADD-TODOLIST':
+            return {
+                ...state,
+                [v1()]: []
+            }
+        // return {...state[v1()]:[], state[v1()]=[]
+        // }
         default:
             return state
 
@@ -49,3 +71,8 @@ export const addTaskAC = (title: string, todolistId: string) => {
 export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string) => {
     return {type: 'CHANGE-TASK-STATUS', taskId, isDone, todolistId} as const
 }
+
+export const changeTaskTitleAC = (taskId: string, title: string, todolistId: string) => {
+    return {type: 'CHANGE-TASK-TITLE', taskId, title, todolistId} as const
+}
+
