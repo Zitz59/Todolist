c23949bffd07a2c91b466790f70a6fead20fff6e
Zitz59 <dondongudon@gmail.com>
2022-07-24 15:29:44 +0300

    refactor: change todolist id to id

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -5,25 +5,21 @@ const settings = {
     headers: {'API-KEY': '6b74dff9-4a4a-410f-be7c-0e00f1a06ec7'}
 }
 
-export  const  todolistsApi = {
+export const todolistsApi = {
 
-    getTodolists(){
-        const promise =   axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+    getTodolists() {
+        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
         return promise
-
     },
 
-    createTodolist(title:string){
+    createTodolist(title: string) {
         return axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: title}, settings)
     },
 
-    deleteTodolist(todolistID:string){
-        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, settings)
+    deleteTodolist(id: string) {
+        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, settings)
     },
-    updateTodolist(todolistID:string, title:string){
-        return   axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, {title:title}, settings)
+    updateTodolist(id: string, title: string) {
+        return axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, {title: title}, settings)
     }
-
-
-
 }
