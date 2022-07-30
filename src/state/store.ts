9ab4b9f6fc226d04164b21d6e6f35509f0c7e7f0
Zitz59 <dondongudon@gmail.com>
2022-07-30 13:37:51 +0300

    refactor: add correct types for thunk and dispatch/selector

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,7 @@
-import {tasksReducer} from './task-reducer';
-import {todolistsReducer} from './todolists-reducer';
-import {applyMiddleware, combineReducers, compose, legacy_createStore} from 'redux';
-import thunkMiddleware from 'redux-thunk'
+import {TaskActionsType, tasksReducer} from './task-reducer';
+import {TodolistReducerType, todolistsReducer} from './todolists-reducer';
+import {AnyAction, applyMiddleware, combineReducers, compose, legacy_createStore} from 'redux';
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 
 declare global {
     interface Window {
@@ -15,14 +15,17 @@ const rootReducer = combineReducers({
     todolists: todolistsReducer
 })
 
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // непосредственно создаём store export const store = legacy_createStore(rootReducer, composeEnhancers());
 
-
 // непосредственно создаём store
-export const store = legacy_createStore(rootReducer,applyMiddleware(thunkMiddleware));
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunkMiddleware),);
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
+
+type AppActionsType = TodolistReducerType | TaskActionsType
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionsType>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
